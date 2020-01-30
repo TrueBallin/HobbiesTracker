116de014d94a4669eabc222040a5afe8bd7ea88b
TrueBallin <ballin_true@yahoo.com>
2020-01-29 16:27:35 -0800

    Initial Commit

---
+++ UITable1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITable1
+//
+//  Created by True Ballin on 1/29/20.
+//  Copyright © 2020 True Ballin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
